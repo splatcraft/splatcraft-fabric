e53f6a2fd7deb88cf9ae9f49714aaf36aab4eabb
Daniel Orr <orrdaniel73@gmail.com>
2021-03-06 13:32:33 +0000

    fix ink tanks being breakable

---
+++ src/main/java/com/cibernet/splatcraft/mixin/PlayerInventoryMixin.java
@@ -0,0 +1,21 @@
+package com.cibernet.splatcraft.mixin;
+
+import com.cibernet.splatcraft.item.InkTankArmorItem;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(PlayerInventory.class)
+public class PlayerInventoryMixin {
+    @Inject(method = "damageArmor", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;damage(ILnet/minecraft/entity/LivingEntity;Ljava/util/function/Consumer;)V", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILEXCEPTION, cancellable = true)
+    private void damageArmor(DamageSource damageSource, float amount, CallbackInfo ci, int i, ItemStack itemStack) {
+        if (itemStack.getItem() instanceof InkTankArmorItem) {
+            ci.cancel();
+        }
+    }
+}
