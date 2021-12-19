c2c596f436e938502ae97d3404c12ed4678dcb60
Daniel Orr <orrdaniel73@gmail.com>
2021-12-19 12:40:15 +0000

    extract some event lambdas to method references

--- src/main/java/net/splatcraft/Splatcraft.java
+++ src/main/java/net/splatcraft/Splatcraft.java
@@ -1,12 +1,17 @@
 package net.splatcraft;
 
 import com.google.common.reflect.Reflection;
+import com.mojang.brigadier.CommandDispatcher;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -57,18 +62,9 @@ public class Splatcraft implements ModInitializer {
             NetworkingCommon.class
         );
 
-        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
-            InkColorCommand.register(dispatcher);
-        });
-        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
-            refreshSplatfestBand(handler.player);
-        });
-        ServerTickEvents.START_SERVER_TICK.register(server -> {
-            for (ServerPlayerEntity player : PlayerLookup.all(server)) {
-                PlayerDataComponent data = PlayerDataComponent.get(player);
-                data.setSubmerged(canSubmergeInInk(player));
-            }
-        });
+        CommandRegistrationCallback.EVENT.register(Splatcraft::registerCommands);
+        ServerPlayConnectionEvents.JOIN.register(Splatcraft::onPlayerJoin);
+        ServerTickEvents.START_SERVER_TICK.register(Splatcraft::onStartTick);
 
         if (FabricLoader.getInstance().isDevelopmentEnvironment()) initDev();
 
@@ -86,5 +82,20 @@ public class Splatcraft implements ModInitializer {
         }
 
         LOGGER.info("Initialized {}-dev", Splatcraft.MOD_NAME);
+    }
+
+    private static void registerCommands(CommandDispatcher<ServerCommandSource> dispatcher, boolean dedicated) {
+        InkColorCommand.register(dispatcher);
+    }
+
+    private static void onPlayerJoin(ServerPlayNetworkHandler handler, PacketSender sender, MinecraftServer server) {
+        refreshSplatfestBand(handler.player);
+    }
+
+    private static void onStartTick(MinecraftServer server) {
+        for (ServerPlayerEntity player : PlayerLookup.all(server)) {
+            PlayerDataComponent data = PlayerDataComponent.get(player);
+            data.setSubmerged(canSubmergeInInk(player));
+        }
     }
 }
