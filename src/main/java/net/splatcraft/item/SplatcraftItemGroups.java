c5211b54f5822563704c4147506d7dddaf9e4f42
Daniel Orr <orrdaniel73@gmail.com>
2021-12-27 03:29:34 +0000

    change inkables item group icon to be an inkwell

--- src/main/java/net/splatcraft/item/SplatcraftItemGroups.java
+++ src/main/java/net/splatcraft/item/SplatcraftItemGroups.java
@@ -6,19 +6,13 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.splatcraft.Splatcraft;
 import net.splatcraft.block.SplatcraftBlocks;
-import net.splatcraft.inkcolor.InkColors;
-import net.splatcraft.inkcolor.Inkable;
 
 import java.util.function.Supplier;
 
 public class SplatcraftItemGroups {
     public static final ItemGroup ALL = register("all", () -> new ItemStack(SplatcraftBlocks.CANVAS));
 
-    public static final ItemGroup INKABLES = register("inkables", () -> {
-        ItemStack stack = new ItemStack(SplatcraftBlocks.CANVAS);
-        Inkable.class.cast(stack).setInkColor(InkColors.random());
-        return stack;
-    });
+    public static final ItemGroup INKABLES = register("inkables", () -> new ItemStack(SplatcraftBlocks.INKWELL));
 
     private static ItemGroup register(String id, Supplier<ItemStack> item) {
         return FabricItemGroupBuilder.build(new Identifier(Splatcraft.MOD_ID, id), item);
