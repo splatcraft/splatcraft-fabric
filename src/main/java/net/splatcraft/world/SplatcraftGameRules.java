f0dc4c9bcec8bf53fc0ec0bf60748d3c187f6276
Daniel Orr <orrdaniel73@gmail.com>
2021-12-30 15:38:53 +0000

    remove duplicated code from SplatcraftGameRules

--- src/main/java/net/splatcraft/world/SplatcraftGameRules.java
+++ src/main/java/net/splatcraft/world/SplatcraftGameRules.java
@@ -17,13 +17,17 @@ public class SplatcraftGameRules {
             .formatted(Formatting.YELLOW, Formatting.BOLD)
     );
 
-    public static final Key<BooleanRule> SPLATFEST_BAND_MUST_BE_HELD = register("splatfestBandMustBeHeld", GameRuleFactory.createBooleanRule(true));
-    public static final Key<BooleanRule> INKWELL_CHANGES_INK_COLOR = register("inkwellChangesInkColor", GameRuleFactory.createBooleanRule(false));
-    public static final Key<BooleanRule> HURT_INK_SQUIDS_IN_WATER = register("hurtInkSquidsInWater", GameRuleFactory.createBooleanRule(true));
-    public static final Key<BooleanRule> HURT_INK_SQUIDS_ON_ENEMY_INK = register("hurtInkSquidsOnEnemyInk", GameRuleFactory.createBooleanRule(true));
-    public static final Key<BooleanRule> INK_TANK_INK_REGENERATION = register("inkTankInkRegeneration", GameRuleFactory.createBooleanRule(true));
+    public static final Key<BooleanRule> SPLATFEST_BAND_MUST_BE_HELD = register("splatfestBandMustBeHeld", true);
+    public static final Key<BooleanRule> INKWELL_CHANGES_INK_COLOR = register("inkwellChangesInkColor", false);
+    public static final Key<BooleanRule> HURT_INK_SQUIDS_IN_WATER = register("hurtInkSquidsInWater", true);
+    public static final Key<BooleanRule> HURT_INK_SQUIDS_ON_ENEMY_INK = register("hurtInkSquidsOnEnemyInk", true);
+    public static final Key<BooleanRule> INK_TANK_INK_REGENERATION = register("inkTankInkRegeneration", true);
 
     private static <T extends Rule<T>> Key<T> register(String id, Type<T> type) {
         return GameRuleRegistry.register("%s:%s".formatted(Splatcraft.MOD_ID, id), SplatcraftGameRules.CATEGORY, type);
+    }
+
+    private static Key<BooleanRule> register(String id, boolean defaultValue) {
+        return register(id, GameRuleFactory.createBooleanRule(defaultValue));
     }
 }
