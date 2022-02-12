bf2c50bd18730169a2e1504c84922a66c7c94fc9
andantet <orrdaniel73@gmail.com>
2022-02-12 18:35:12 +0000

    implement custom color blender instead of defaulting to flat (sodium compat)

--- src/main/java/net/splatcraft/mixin/client/compat/SodiumBiomeBlendFixMixin.java
+++ src/main/java/net/splatcraft/mixin/client/compat/SodiumBiomeBlendFixMixin.java
@@ -1,9 +1,10 @@
 package net.splatcraft.mixin.client.compat;
 
 import me.jellysquid.mods.sodium.client.model.quad.blender.ColorBlender;
-import me.jellysquid.mods.sodium.client.model.quad.blender.FlatColorBlender;
+import me.jellysquid.mods.sodium.client.model.quad.blender.LinearColorBlender;
 import me.jellysquid.mods.sodium.client.render.pipeline.ChunkRenderCache;
 import net.splatcraft.client.config.ClientCompatConfig;
+import net.splatcraft.client.model.quad.blender.InkColorBlender;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,8 +15,11 @@ public class SodiumBiomeBlendFixMixin {
     /**
      * Disables smooth biome blender if configured.
      */
-    @Inject(method = "createBiomeColorBlender", at = @At("HEAD"), cancellable = true, remap = false)
+    @Inject(method = "createBiomeColorBlender", at = @At("RETURN"), cancellable = true, remap = false)
     private void disableSmoothBlending(CallbackInfoReturnable<ColorBlender> cir) {
-        if (ClientCompatConfig.INSTANCE.sodium_inkBiomeBlendFix.getValue()) cir.setReturnValue(new FlatColorBlender());
+        if (ClientCompatConfig.INSTANCE.sodium_inkBiomeBlendFix.getValue()) {
+            ColorBlender blender = cir.getReturnValue();
+            if (blender instanceof LinearColorBlender) cir.setReturnValue(new InkColorBlender());
+        }
     }
 }
